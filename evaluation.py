7bf0cf61e7c613e0ba37346794e8d4c2475e53a4
Jivl00 <vladimira.kimlova@seznam.cz>
2024-05-05 17:42:35 +0200

    evaluation finished

--- evaluation.py
+++ evaluation.py
@@ -8,8 +8,8 @@ from searcher import search
 
 # PIPELINE
 # pipeline = preprocessing_pipelines.pipeline_stemmer
-# pipeline = preprocessing_pipelines.pipeline_lemmatizer
-pipeline = preprocessing_pipelines.pipeline_lemmatizer2
+pipeline = preprocessing_pipelines.pipeline_lemmatizer
+# pipeline = preprocessing_pipelines.pipeline_lemmatizer2
 
 
 def load_queries():
@@ -21,7 +21,8 @@ def load_queries():
     queries_file = "eval_data/czechTopics.csv"
     df = pd.read_csv(queries_file, delimiter="|", encoding="utf-8")
     for index, row in df.iterrows():
-        queries[row["Id"]] = row["Title"] # TODO: try other fields
+        # queries[row["Id"]] = row["Title"] + " " + row["Description"]
+        queries[row["Id"]] = row["Description"]
     return queries
 
 
@@ -66,7 +67,7 @@ eval_docs = load_documents()
 time_end = time.time()
 print("Loaded documents in", time_end - time_start, "seconds")
 time_start = time.time()
-eval_index = Index(pipeline, "eval_index_lem2", "eval_index")
+eval_index = Index(pipeline, "eval_index_lem", "eval_index")
 
 # create_doc_cache(eval_index, eval_docs)
 # preped_docs = []
@@ -101,8 +102,10 @@ for query_id in queries.keys():
         true_doc_id = eval_index.docs["docs"][result.doc_id]["id"]
         line = query_id + " Q0 " + true_doc_id + " " + str(rank + 1) + " " + str(result.score) + " runindex1"
         eval_results += line + "\n"
+time_end = time.time()
+print("Search took", time_end - time_start, "seconds")
 
-with open("eval_data/eval_results_lem2.txt", "w", encoding="utf-8") as file:
+with open("eval_data/eval_results_lem_des.txt", "w", encoding="utf-8") as file:
     file.write(eval_results)
 
 
@@ -115,8 +118,47 @@ Loaded 81735 documents
 Preprocessed documents in 478.4098148345947 seconds
 Created index in 94.56351232528687 seconds
 Saved index in 112.15081214904785 seconds
-
+Loaded index in 36.31753921508789 seconds
 map             all     0.0895
+
+whole query
+map             all     0.0997
+description only
+map             all     0.0684
+
+With stopwords:
+Preprocessed documents in 297.74697828292847 seconds
+Created index in 150.09004592895508 seconds
+Saved index in 147.52977061271667 seconds
+Loaded index in 122.91806960105896 seconds
+map             all     0.0864
+
+
+
+LEMMATIZATION - czech and slovak
+---------------------------------------
+Loaded 81735 documents
+Preprocessed documents in 368.571417093277 seconds
+Created index in 98.80106329917908 seconds
+Saved index in 116.73172044754028 seconds
+Loaded index in 83.92605090141296 seconds
+Search took 11.905768632888794 seconds
+
+map             all     0.0283
+
+whole query
+map             all     0.0099
+description only
+map             all     0.0102
+
+
+With stopwords:
+Preprocessed documents in 179.4125566482544 seconds
+Created index in 141.34408688545227 seconds
+Saved index in 145.90315890312195 seconds
+Loaded index in 100.11580419540405 seconds
+map             all     0.0224
+
 
 LEMMATIZATION lemmagen3 
 ---------------------------------------
@@ -127,17 +169,6 @@ Saved index in 116.01357436180115 seconds
 Loaded index in 74.79287338256836 seconds
 
 map             all     0.0176
-
-
-LEMMATIZATION - czech and slovak
----------------------------------------
-Loaded 81735 documents
-Preprocessed documents in 368.571417093277 seconds
-Created index in 98.80106329917908 seconds
-Saved index in 116.73172044754028 seconds
-Loaded index in 83.92605090141296 seconds
-
-map             all     0.0283
 
 """
 
